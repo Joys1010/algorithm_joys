0630e5b375b29831b435fbc1cdb556a20c11dcab
Joys1010 <joyslee1010@gmail.com>
2021-08-04 02:58:46 +0900

    210803 : #9251

---
+++ 2021_8/9251.cpp
@@ -0,0 +1,38 @@
+#include<iostream>
+#include<algorithm>
+
+using namespace std;
+
+char first_string[1001];
+char second_string[1001];
+
+int dp[1001][1001];
+
+int main()
+{
+	
+	cin >> first_string >> second_string;
+
+	int i = 0;
+	int j = 0;
+	int maxi = 0;
+		
+	while (second_string[i] != '\0') {
+		while (first_string[j] != '\0') {
+			if (second_string[i] == first_string[j]) {
+				dp[i+1][j+1] = dp[i][j] + 1;
+			}
+			else {
+				dp[i+1][j+1] = max(dp[i+1][j], dp[i][j+1]);
+			}
+			if (maxi < dp[i + 1][j + 1]) {
+				maxi = dp[i + 1][j + 1];
+			}
+			j++;
+		}
+		i++;
+		j = 0;
+    }
+	cout <<maxi << "\n";
+	return 0;
+}
